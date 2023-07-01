c9b539794c210a8bb6269e07780e088d258c62d8
wesley <w.santos.s@hotmail.com>
2023-06-30 22:25:33 -0300

    terceiro commit

--- controller/TaskController.js
+++ controller/TaskController.js
@@ -1,7 +1,21 @@
-const Agenda = (req,res) => {
-   return res.render ("index")
-};
 
-module.exports = {
-    Agenda,
-}
+
+const schedule = [
+    [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null]
+  ];
+  
+  
+  const Agenda = (req, res) => {
+    return res.render('index', { schedule: schedule });
+  };
+  
+  module.exports = {
+    Agenda
+  };
+  
--- public/script.js
+++ public/script.js
@@ -1,1 +1,20 @@
-alert("")
+const schedule = [
+    [], // segunda-feira
+    [], // terça-feira
+    [], // quarta-feira
+    [], // quinta-feira
+    [], // sexta-feira
+    []  // sábado
+  ];
+  
+  document.addEventListener('DOMContentLoaded', () => {
+    const inputs = document.querySelectorAll('input[type="text"]');
+    inputs.forEach((input) => {
+      input.addEventListener('change', (event) => {
+        const [day, hour] = event.target.id.split('-').slice(1);
+        const value = event.target.value;
+        schedule[day][hour] = { name: value };
+      });
+    });
+  });
+  
